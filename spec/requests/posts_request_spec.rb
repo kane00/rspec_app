cbc52c277dde373acb56ec884007bd9b5062de2d
kane00 <nkaneko00@gmail.com>
2020-08-18 07:03:49 +0000

    controller model

---
+++ spec/requests/posts_request_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe "Posts", type: :request do
+  describe '新規投稿ページ' do
+    context "新規投稿ページが正しく表示される" do
+      before do
+        get posts_new_path
+      end
+      it 'リクエストは200 OKとなること' do
+        expect(response.status).to eq 200
+      end
+      # ページにタイトルが正しく表示されているかテスト追加
+      it 'タイトルが正しく表示されていること' do
+        expect(response.body).to include("新規投稿")
+      end
+    end
+  end
+end
+
+
+# コードの解説：
+# describe：PostsControllerをテスト対象にすることを示している。
+# describe：新規投稿ページの関するテストであることを示している。
+# context ：新規投稿ページの正しく表示されることに関するものをまとめる。
+# before ：今回は' get posts_new_path 'で新規投稿ページを表示する処理をitの前に行う。
+# response.status：この記述することでレスポンスステータスが受け取ることができる。
+# eq ：表示成功した時のレスポンスステータスの' 200 'を記述している
+# response.body：htmlのデータが入っている
+# include：()の中身が含まれているか検証している
---
+++ spec/test_rspec.rb
@@ -0,0 +1,20 @@
+describe '四則演算' do
+  context '足し算' do
+    it '1 + 1 は 2 になる' do
+      expect(1 + 1).to eq 2
+    end
+  end
+  context '足し算' do
+    it '1 + 1 は 2 になる' do
+      expect(1 + 1).to eq 3
+    end
+  end
+end
+
+# このテストコードは以下の構成になっています。
+# describe '四則演算のテストのグループ'
+# context '足し算のテストをまとめている'
+# it 'テストと具体的な内容を記述する'
+# expect 'expectは処理内容などを記述する'
+# to '期待している値が「~であること」を意味しています。(逆の場合はnot_toを使用します)'
+# eq 'eq(イコール)は期待している内容を記述する'
